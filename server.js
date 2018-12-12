d70b3892862091cf5c0e75592ac3f877e7c5fd02
Jonah Martinez <jonah.martinez01@outlook.com>
2018-12-11 20:34:13 -0500

    Modified POST request to work using DB

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var port = 3000;
 
 app.get('/', function(request, response){
     response.send("Get request recieved at '/'")
-})
+});
 
 app.get('/quotes', function(request, response){
     if (request.query.year) {
@@ -23,21 +23,21 @@ app.get('/quotes', function(request, response){
               response.send(err.message);
             }
             else{
-                console.log("Return a list of quotes from the year: " + req.query.year);
-                res.json(rows);
+                console.log("Return a list of quotes from the year: " + request.query.year);
+                response.json(rows);
               }
-          })
+          });
     }
     else{
         db.all('SELECT * FROM quotes', function processRows(err, rows){
             if(err){
-                res.send(err.message);
+                response.send(err.message);
             }
             else{
                 for( var i = 0; i < rows.length; i++){
                     console.log(rows[i].quote);
                 }
-                res.json(rows);
+                response.json(rows);
             }
         });
     }
@@ -45,7 +45,7 @@ app.get('/quotes', function(request, response){
 
 app.get('/quotes/:id', function(request, response){
     console.log("return quote with ID: " + request.param.id)
-    db.get('SELECT * FROM Contacts WHERE rowid = ?', [request.params.id], function(err, row){
+    db.get('SELECT * FROM quotes WHERE rowid = ?', [request.params.id], function(err, row){
         if(err){
           console.log("ERROR: " + err.message);
         }
@@ -53,14 +53,23 @@ app.get('/quotes/:id', function(request, response){
           response.json(row);
         }
       });
-})
+});
 
 app.post('/quotes', function(request, response){
     console.log("Insert a new quote: " + request.body.quote);
-    response.json(request.body);
-})
+    db.run('INSERT INTO quotes VALUES (?,?,?)', [request.body.quote, 
+        request.body.author, 
+        request.body.year], function(err){
+        if(err){
+            console.send(err.message);
+        }
+        else{
+            response.send('Inserted quote with id: ' + this.lastID);
+        }
+    });
+});
 
 
 app.listen(port, function () {
     console.log('Express app listening on port + ' + port);
-})
+});
